3d0784f7df8408a13466ae737b2e902635011c1c
Bret Axl Sebastian Pura <cosmicdevcs@gmail.com>
2021-11-28 20:56:24 +0800

    feature: added video room monitor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { getToken } from './helpers'
 import { connect } from 'twilio-video'
+import { VideoRoomMonitor } from '@twilio/video-room-monitor'
 
 const IDENTITY = 'USER_1'
 const ROOM_NAME = 'DEFAULT_ROOM'
@@ -13,8 +14,11 @@ function App() {
         const result = await getToken(IDENTITY)
         console.log('Access Token:', result.data.accessToken)
 
-        const room = await connect(result.data.accessToken, { name: ROOM_NAME })
+        const room = await connect(result.data.accessToken, { name: ROOM_NAME, tracks: [] })
         console.log('Twilio Room Instance:', room)
+
+        // Register the room
+        VideoRoomMonitor.registerVideoRoom(room)
       } catch (err) {
         console.log(err)
       }
@@ -22,9 +26,15 @@ function App() {
     init()
   }, [])
 
+  const openRoomMonitor = () => {
+    VideoRoomMonitor.openMonitor()
+  }
+
   return (
     <div>
       <h1>{ process.env.REACT_APP_TWILIO_SERVERLESS_URL }</h1>
+
+      <button onClick={() => openRoomMonitor()}>Room Monitor</button>
     </div>
   );
 }
